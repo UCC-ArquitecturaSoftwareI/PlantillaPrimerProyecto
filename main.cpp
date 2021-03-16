180f9569e41cc5e5860e6ba9fa5c11efa90d4c16
Ricardo Martín Marcucci <rmarku@gmail.com>
2021-03-15 21:06:31 -0300

    Test second CI

--- main.cpp
+++ main.cpp
@@ -1,67 +1,49 @@
-/*********************************************************************
-(c) Matt Marchant & contributors 2016 - 2019
-http://trederia.blogspot.com
-
-tmxlite - Zlib license.
-
-This software is provided 'as-is', without any express or
-implied warranty. In no event will the authors be held
-liable for any damages arising from the use of this software.
-
-Permission is granted to anyone to use this software for any purpose,
-including commercial applications, and to alter it and redistribute
-it freely, subject to the following restrictions:
-
-1. The origin of this software must not be misrepresented;
-you must not claim that you wrote the original software.
-If you use this software in a product, an acknowledgment
-in the product documentation would be appreciated but
-is not required.
-
-2. Altered source versions must be plainly marked as such,
-and must not be misrepresented as being the original software.
-
-3. This notice may not be removed or altered from any
-source distribution.
-*********************************************************************/
-
-#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics.hpp>
 #include <SFML/Window/Event.hpp>
-
+#include <iostream>
+#include <tmxlite/Layer.hpp>
 #include <tmxlite/Map.hpp>
+#include <tmxlite/TileLayer.hpp>
 
-#include "SFMLOrthogonalLayer.hpp"
+int main() {
+  sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+  std::vector<sf::Sprite> map_sprites;
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-
-    tmx::Map map;
-    map.load("assets/demo.tmx");
-
-    MapLayer layerZero(map, 0);
-    MapLayer layerOne(map, 1);
-    MapLayer layerTwo(map, 2);
-
-    sf::Clock globalClock;
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
+  tmx::Map map;
+  if (map.load("assets/mapas/mapa.tmx")) {
+    std::cout << "encontré mapa" << std::endl;
+    const auto &layers = map.getLayers();
+    for (const auto &layer : layers) {
+      std::cout << "encontré layer " << layer->getName() << std::endl;
+      if (layer->getType() == tmx::Layer::Type::Object) {
+        const auto &objectLayer = layer->getLayerAs<tmx::ObjectGroup>();
+        const auto &objects = objectLayer.getObjects();
+        for (const auto &object : objects) {
+          std::cout << object.getName() << std::endl;
         }
 
-        sf::Time duration = globalClock.restart();
-        layerZero.update(duration);
+      } else if (layer->getType() == tmx::Layer::Type::Tile) {
+        const auto &tileLayer = layer->getLayerAs<tmx::TileLayer>();
+      }
+      const auto &tilesets = map.getTilesets();
+      for (const auto &tileset : tilesets) {
+        std::cout << tileset.getName() << std::endl;
+      }
+    }
+  }
 
-        window.clear(sf::Color::Black);
-        window.draw(layerZero);
-        window.draw(layerOne);
-        window.draw(layerTwo);
-        window.display();
+  sf::Clock globalClock;
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) window.close();
     }
 
-    return 0;
+    // sf::Time duration = globalClock.restart();
+
+    window.clear(sf::Color::Black);
+    window.display();
+  }
+
+  return 0;
 }
